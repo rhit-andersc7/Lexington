e37d202dfcee54b80e7adfa5c3dcd69d43c8453e
Cullen LaKemper <lakempca@rose-hulman.edu>
2021-03-25 20:34:42 -0400

    added function definitions

--- InputOutput/kernel.c
+++ InputOutput/kernel.c
@@ -1,27 +1,9 @@
 void printString(char *);
+void printCharLocation(char, int, int);
+void printStringLocation(char*, int, int);
 int main() {
-	putInMemory(0xB000, 0x8500, 'H');
-	putInMemory(0xB000, 0x8501, 0x7);
-	putInMemory(0xB000, 0x8502, 'e');
-	putInMemory(0xB000, 0x8503, 0x7);
-	putInMemory(0xB000, 0x8504, 'l');
-	putInMemory(0xB000, 0x8505, 0x7);
-	putInMemory(0xB000, 0x8506, 'l');
-	putInMemory(0xB000, 0x8507, 0x7);
-	putInMemory(0xB000, 0x8508, 'o');
-	putInMemory(0xB000, 0x8509, 0x7);
-	putInMemory(0xB000, 0x850A, ' ');
-	putInMemory(0xB000, 0x850B, 0x7);
-	putInMemory(0xB000, 0x850C, 'W');
-	putInMemory(0xB000, 0x850D, 0x7);
-	putInMemory(0xB000, 0x850E, 'o');
-	putInMemory(0xB000, 0x850F, 0x7);
-	putInMemory(0xB000, 0x8510, 'r');
-	putInMemory(0xB000, 0x8511, 0x7);
-	putInMemory(0xB000, 0x8512, 'l');
-	putInMemory(0xB000, 0x8513, 0x7);
-	putInMemory(0xB000, 0x8514, 'd');
-	putInMemory(0xB000, 0x8515, 0x7);
+	printStringLocation("Hello World", 0, 0);
+	printString("Hello World");
 	while(1) {
 	}
 	return 0;
@@ -37,7 +19,7 @@ void printString(char *chars) {
 	}
 }
 
-void printChar(char c, int row, int column){
+void printCharLocation(char c, int row, int column){
 	int memRow = 0xB000 + row*160;
 	int memCol = memRow + column*2; 
 
@@ -45,13 +27,13 @@ void printChar(char c, int row, int column){
 	putInMemory(memRow,memCol+1, 0x7);
 }
 
-void printString(char* string, int row, int column){
+void printStringLocation(char* string, int row, int column){
 
 	int i = 0;
 	char c = string[i];
 
 	while(c!='\0'){
-		printChar(c,row,column);
+		printCharLocation(c,row,column);
 		
 		i++;
 		column++;
