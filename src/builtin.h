732911073a0a2f5d35adf9bb7774ed56e847319c
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-05-03 19:33:46 -0400

    Add initializeProgram() to builtin.h

--- src/builtin.h
+++ src/builtin.h
@@ -4,4 +4,5 @@ void makeTimerInterrupt();
 void putInMemory(int segment, int address, char character);
 void enableInterrupts();
 void launchProgram(int segment);
+void initializeProgram(int segment);
 void returnFromTimer(int segment, int sp);
